65e2df7de22495df6a4b8433ce9178426d5753c2
cuneguines <47780327+cuneguines@users.noreply.github.com>
2023-05-03 10:52:00 +0100

    commit

--- VISUAL MANAGEMENT SYSTEM/LIVE/Livetracker_1_3_0/LIVETRACKER/PW_TEST/enter_password.php
+++ VISUAL MANAGEMENT SYSTEM/LIVE/Livetracker_1_3_0/LIVETRACKER/PW_TEST/enter_password.php
@@ -14,7 +14,7 @@
         <div style="margin-top:20%">
             <div style = "width:30%; height:20%; position:relative; vertical-align:top; top:40%; left: 35%; background-color:#ff5c33;">
                 <form style = "height:100%;background: linear-gradient(#606062, #28262b); margin:0;" action = "./test.php" method="post">
-                    <p style =  "text-transform:uppercase;color:white; font-size:20px; margin-bottom:30px;"><br>Enter Userid & Password :</p>
+                    <p style =  "text-transform:uppercase;color:white; font-size:20px; margin-bottom:30px;"><br>Enter Userid & Passworddd :</p>
                     <input style = "font-size:20px; width:30%; height:30%;" type="text" name="user" placeholder = "user"></input>
                     <input style = "font-size:20px; width:30%; height:30%;" type="password" name="password" placeholder = "password"></input>
                     <input style = "font-size:20px; width:20%; height:30%;" type = "submit" value = "submit">
