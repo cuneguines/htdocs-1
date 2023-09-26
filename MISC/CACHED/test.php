7e532f3554f339f84522edfc4943d0d4c8e24820
cuneguines <47780327+cuneguines@users.noreply.github.com>
2023-09-26 15:36:06 +0100

    coomit

---
+++ MISC/CACHED/test.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<style>
+
+body {
+    font-family: Arial, sans-serif;
+}
+
+.tree {
+    list-style-type: none;
+    padding-left: 20px;
+}
+
+.tree .step {
+    background-color: #f0f0f0;
+    border: 1px solid #ccc;
+    padding: 5px;
+    border-radius: 5px;
+}
+
+.tree li {
+    margin-top: 5px;
+}
+
+.tree ul {
+    list-style-type: none;
+    padding-left: 20px;
+    border-left: 1px dashed #ccc;
+    margin-top: 5px;
+}
+
+.tree ul ul {
+    border-left: none;
+}
+</style>
+<body>
+    <ul class="tree">
+        <li>
+            <span class="step">130020890 - 110mm OD x 1.5mm Wall x 75mm Longit pipe 304</span>
+            <ul>
+                <li><span class="step">FABRICATION</span></li>
+                <li><span class="step">Machined M16 x 145mm Hex Head With Triangular Insert - KS068560</span></li>
+                <li>
+                    <span class="step">IGU Unit - 450 x 600 - 450mm Deep - 316 s/s - Dual Water</span>
+                    <ul>
+                        <li><span class="step">LASER LABOUR</span></li>
+                        <li><span class="step">LASER MACHINE</span></li>
+                        <li><span class="step">BRAKE PRESS</span></li>
+                        <li><span class="step">FABRICATION</span></li>
+                        <li><span class="step">PICKLE & PASSIVATE</span></li>
+                        <li><span class="step">PACKAGING HOURS</span></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</body>
+</html>
